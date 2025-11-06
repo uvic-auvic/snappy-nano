aba50a9014f34769cd022dc96bfe8bd5c5a46753
megan <megand0905@gmail.com>
2025-11-05 19:54:46 -0800

    Added controller node

--- src/snappy_cpp/src/controller.cpp
+++ src/snappy_cpp/src/controller.cpp
@@ -1,1 +1,201 @@
-// this file will have all the controller logic for the submarine
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "snappy_cpp/msg/task.hpp"
+#include "snappy_cpp/msg/pose.hpp"
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <queue>
+
+using namespace std::chrono_literals;
+using std::placeholders::_1;
+
+class Controller : public rclcpp::Node {
+    public: 
+        Controller() : Node("controller") {
+            // Publish state status to planner
+            status_publisher_ = this->create_publisher<std_msgs::msg::String>("/controller/status", 10);
+
+            // Publish trajectory vectors to state estimator
+            trajectory_publisher_ = this->create_publisher<snappy_cpp::msg::Pose>("/controller/trajectory", 10);
+            
+            // Receive tasks from planner
+            task_subscription_ = this->create_subscription<snappy_cpp::msg::Task>(
+                "/planner/task", 10, std::bind(&Controller::task_callback, this, _1));
+                
+            // Receive states from state estimator
+            state_subscription_ = this->create_subscription<snappy_cpp::msg::Pose>(
+                "state_estimator/state", 10, std::bind(&Controller::state_callback, this, _1));
+
+            // Publish every 100ms ??
+            status_timer_ = this->create_wall_timer(
+                100ms, std::bind(&Controller::status_callback, this));
+            trajectory_timer_ = this->create_wall_timer(
+                100ms, std::bind(&Controller::trajectory_callback, this));
+        }
+
+    private:
+        // Publish state to planner
+        void status_callback() {
+            auto status_message = std_msgs::msg::String();
+            auto [position_error, orientation_error ] = computeError();
+
+            if (position_error == -1 && orientation_error == -1) {
+                status_message.data = "Fail";
+            } else if ((position_error < error_threshold) && (orientation_error < error_threshold)) {
+                status_message.data = "Success";
+                in_progress_ = false;
+            } else {
+                status_message.data = "In progress";
+            }
+            
+            status_publisher_->publish(status_message);
+            RCLCPP_INFO(this->get_logger(), "Publishing status: '%s'", status_message.data.c_str());
+        }
+        
+        // Publish trajectory to state estimator
+        void trajectory_callback() {
+            auto trajectory_msg = snappy_cpp::msg::Pose();
+
+            if (position_target_.has_value() && orientation_target_.has_value()) {
+                trajectory_msg.position = *position_target_;
+                trajectory_msg.orientation = *orientation_target_;
+
+                trajectory_publisher_->publish(trajectory_msg);
+                RCLCPP_INFO(this->get_logger(), "Publishing trajectory: (%.2f, %.2f, %.2f)",
+                    position_target_->x, position_target_->y, position_target_->z);
+            }
+        }
+
+        // Receive tasks from planner node
+        void task_callback(const snappy_cpp::msg::Task & msg) {
+            RCLCPP_INFO(this->get_logger(), "Received task (%s, %s, %f)",
+                msg.type.c_str(), msg.direction.c_str(), msg.magnitude);
+            
+            if (msg.overwrite) { // discard the current task(s) and start this one
+                std::queue<snappy_cpp::msg::Task> empty;
+                std::swap(tasks_, empty);
+                current_task_ = msg;
+                parseTask(*current_task_);
+                in_progress_ = true;
+                return;
+            }
+            
+            tasks_.push(msg); // put task in queue
+            
+            if (!in_progress_ && !tasks_.empty()) { // if done executing, move onto next task
+                current_task_ = tasks_.front();
+                tasks_.pop();
+                parseTask(*current_task_);
+                in_progress_ = true;
+            }
+        }
+
+        // Receive states from state estimator nodes
+        void state_callback(const snappy_cpp::msg::Pose & msg) {
+            RCLCPP_INFO(this->get_logger(), "Received current position: (%.2f, %.2f, %.2f)",
+            msg.position.x, msg.position.y, msg.position.z);
+            
+            position_current_ = msg.position;
+            orientation_current_ = msg.orientation;
+        }
+
+        // Compute difference between current and target
+        std::pair<double, double> computeError() {
+            if (!position_target_.has_value() || !orientation_target_.has_value()) { // if no target received:
+                return {-1.0, -1.0};
+            }
+            
+            tf2::Quaternion q_current;
+            tf2::Quaternion q_target;
+            tf2::fromMsg(orientation_current_, q_current);
+            tf2::fromMsg(*orientation_target_, q_target);
+            return {
+                // position_error
+                (std::sqrt(std::pow(position_current_.x - position_target_->x, 2)
+                + std::pow(position_current_.y - position_target_->y, 2)
+                + std::pow(position_current_.z - position_target_->z, 2))
+                ),
+                // orientation_error
+            q_current.angleShortestPath(q_target)
+            };
+        }
+
+
+        void parseTask(const snappy_cpp::msg::Task & msg) {
+            // If there is no current target:
+            if (!position_target_.has_value()) {
+                position_target_ = position_current_;
+            }
+            if (!orientation_target_.has_value()) {
+                orientation_target_ = orientation_current_;
+            }
+
+            if (msg.type == "move") {
+                // Direction commands
+                if (msg.direction == "x" || msg.direction == "y" || msg.direction == "z") {
+                    // if absolute: target = magnitude, 
+                    // else: target = magnitude + current_position
+                    if (msg.direction == "x") {
+                        position_target_->x = msg.absolute ? msg.magnitude : msg.magnitude + position_current_.x;
+                    } else if (msg.direction == "y") {
+                        position_target_->y = msg.absolute ? msg.magnitude : msg.magnitude + position_current_.y;
+                    } else {
+                        position_target_->z = msg.absolute ? msg.magnitude : msg.magnitude + position_current_.z;
+                    }
+
+                // Orientation commands
+                } else {
+                    tf2::Quaternion q_current;
+                    tf2::fromMsg(orientation_current_, q_current);
+                    tf2::Quaternion q_temp;
+
+                    if (msg.direction == "yaw") {
+                        q_temp.setRPY(0, 0, msg.magnitude);
+                    } else if (msg.direction == "roll") {
+                        q_temp.setRPY(msg.magnitude, 0, 0);
+                    } else if (msg.direction == "pitch") {
+                        q_temp.setRPY(0, msg.magnitude, 0);
+                    } else {
+                        RCLCPP_ERROR(this->get_logger(), "Invalid orientation '%s'", msg.direction.c_str());
+                    }
+                    orientation_target_ = tf2::toMsg(msg.absolute ? q_temp : q_current * q_temp);
+                }
+            } else {
+                // type = dropper/grabber/torpedo ??
+                if (msg.direction == "on") {} else if (msg.direction == "off") {
+                } else {
+                    RCLCPP_ERROR(this->get_logger(), "Invalid direction '%s'", msg.direction.c_str());
+                }
+            }
+        }
+        
+        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr status_publisher_;
+        rclcpp::Publisher<snappy_cpp::msg::Pose>::SharedPtr trajectory_publisher_;
+        rclcpp::Subscription<snappy_cpp::msg::Task>::SharedPtr task_subscription_; 
+        rclcpp::Subscription<snappy_cpp::msg::Pose>::SharedPtr state_subscription_;
+        rclcpp::TimerBase::SharedPtr status_timer_;
+        rclcpp::TimerBase::SharedPtr trajectory_timer_;
+        
+        std::queue<snappy_cpp::msg::Task> tasks_;
+        std::optional<snappy_cpp::msg::Task> current_task_;
+        bool in_progress_ = false; // set to true in task_callback, reset to false in status_callback
+        
+        geometry_msgs::msg::Point position_current_;
+        std::optional<geometry_msgs::msg::Point> position_target_;
+        geometry_msgs::msg::Quaternion orientation_current_;
+        std::optional<geometry_msgs::msg::Quaternion> orientation_target_;
+        const double error_threshold = 0.005;
+};
+
+  
+
+int main(int argc, char* argv[]) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<Controller>());
+    rclcpp::shutdown();
+    return 0;
+}
